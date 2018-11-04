4cad2af37ec008f11e5c1afe3e8939a7d3baafdd
doublemove <zhongwh1978@gail.com>
2018-11-04 17:46:03 +0800

    use soaplib

--- soaplibTest.py
+++ soaplibTest.py
@@ -1,18 +1,63 @@
 #coding=utf-8
 import sys
 sys.path.append('D:\soaplib-master\src')
+from datetime import datetime
+import pickle, re
 import soaplib
-from soaplib.core.model.primitive import String
+from soaplib.core.model.primitive import String,Integer
 from soaplib.core.server import wsgi
 from soaplib.core.service import DefinitionBase     #所有的服务类都继承DefinitionBase基类
 from soaplib.core.service import soap      #soap标识方法的特性
+from soaplib.core.model.clazz import Array
 
 
 
 class webserver(DefinitionBase):
-    @soap(String, _returns=String)
-    def panDuan(self, date_time, department):
-        return date_time, department
+    @soap(String, String, _returns=Array(Integer))
+    def panDuanTimeAndDept(self, date_time, department):
+        time_flag = 0
+        time_delta = (datetime.strptime(date_time, "%Y-%m-%d") - datetime.now()).days + 1
+        if time_delta > 0 and time_delta < 3:
+            time_flag = 1
+            
+        department_flag = 0
+        yuyue_info = pickle.load(open('data/data', 'r'))
+        print yuyue_info
+        if yuyue_info[department] == '':
+            department_flag = 1
+        
+        return [time_flag, department_flag]
+        
+    @soap(String, String, String, _returns=Array(Integer))
+    def response(self, type, shouji, department):
+        if type == 'yuyue':
+            geshi_flag = 1 if re.match(r'((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$', shouji) else 0
+            yuyue_info = pickle.load(open('data/data', 'r'))
+            shouji_flag = 1 if shouji not in yuyue_info.values() else 0
+            
+            if geshi_flag and shouji_flag:
+                yuyue_info[department] = shouji
+                data = open('data/data', 'w')
+                pickle.dump(yuyue_info, data)
+                data.close()
+        elif type == 'quxiaoyuyue':
+            geshi_flag = 1 if re.match(r'((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$', shouji) else 0
+            yuyue_info = pickle.load(open('data/data', 'r'))
+            shouji_flag = 1 if shouji in yuyue_info.values() else 0
+            
+            if geshi_flag and shouji_flag:
+                if yuyue_info['neike'] == shouji:
+                    yuyue_info['neike'] = ''
+                    print 'neike'
+                elif yuyue_info['waike'] == shouji:
+                    yuyue_info['waike'] = ''
+                    print 'waike'
+                print yuyue_info
+                data = open('data/data', 'w')
+                pickle.dump(yuyue_info, data)
+                data.close()
+        
+        return [geshi_flag, shouji_flag]
 
 if __name__ == '__main__':
     try:
--- tornadoTest.py
+++ tornadoTest.py
@@ -7,6 +7,13 @@ from datetime import datetime
 import pickle
 import re
 
+from suds.client import Client
+
+host = '127.0.0.1'
+port = 7789
+client = Client('http://%s:%s/?wsdl' % (host, port))
+print u'成功连上web service'
+
 if not os.path.exists('data/data'):
     yuyue_info = {'neike': '', 'waike': ''}
     data = open('data/data', 'w')
@@ -37,18 +44,9 @@ class Yuyue02Handler(tornado.web.RequestHandler):
         department = self.get_argument('department')
         print date_time, department
         
-        time_flag = 0
-        time_delta = (datetime.strptime(date_time, "%Y-%m-%d") - datetime.now()).days + 1
-        if time_delta > 0 and time_delta < 3:
-            time_flag = 1
-            
-        department_flag = 0
-        yuyue_info = pickle.load(open('data/data', 'r'))
-        print yuyue_info
-        if yuyue_info[department] == '':
-            department_flag = 1
+        flags = list(client.service.panDuanTimeAndDept(date_time, department))
         
-        self.render('yuyue02.html', time_flag=time_flag, department_flag=department_flag, department=department)
+        self.render('yuyue02.html', time_flag=flags[0][1][0], department_flag=flags[0][1][1], department=department)
 
 class QuxiaoHandler(tornado.web.RequestHandler):
     #添加一个处理get请求方式的方法
@@ -62,36 +60,11 @@ class ResponseHandler(tornado.web.RequestHandler):
         #得到数据
         shouji = self.get_argument('shouji')
         type = self.get_argument('type')
+        department = self.get_argument('department', 'neike')
         
-        if type == 'yuyue':
-            geshi_flag = 1 if re.match(r'((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$', shouji) else 0
-            yuyue_info = pickle.load(open('data/data', 'r'))
-            shouji_flag = 1 if shouji not in yuyue_info.values() else 0
+        flags = list(client.service.response(type, shouji, department))
             
-            if geshi_flag and shouji_flag:
-                department = self.get_argument('department')
-                yuyue_info[department] = shouji
-                data = open('data/data', 'w')
-                pickle.dump(yuyue_info, data)
-                data.close()
-        elif type == 'quxiaoyuyue':
-            geshi_flag = 1 if re.match(r'((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$', shouji) else 0
-            yuyue_info = pickle.load(open('data/data', 'r'))
-            shouji_flag = 1 if shouji in yuyue_info.values() else 0
-            
-            if geshi_flag and shouji_flag:
-                if yuyue_info['neike'] == shouji:
-                    yuyue_info['neike'] = ''
-                    print 'neike'
-                elif yuyue_info['waike'] == shouji:
-                    yuyue_info['waike'] = ''
-                    print 'waike'
-                print yuyue_info
-                data = open('data/data', 'w')
-                pickle.dump(yuyue_info, data)
-                data.close()
-            
-        self.render('response.html', type=type, geshi_flag=geshi_flag, shouji_flag=shouji_flag)
+        self.render('response.html', type=type, geshi_flag=flags[0][1][0], shouji_flag=flags[0][1][1])
 
 if __name__ == '__main__':
     #创建一个应用对象
